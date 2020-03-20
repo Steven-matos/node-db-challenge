ef495d9aa321708a3b2a41933b9b05e02baf4923
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-03-20 13:32:00 -0400

    added router to server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const helmet= require('helmet');
 
+const projRouter = require('../project/project_router');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use("/api/project/", projRouter);
 
 module.exports = server;
