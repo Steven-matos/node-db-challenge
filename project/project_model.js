abe21b07cdcf4f68d729baa153a01ca1fa66bdd5
Steven M <38477361+Steven-matos@users.noreply.github.com>
2020-03-20 13:51:15 -0400

    Revert "Revert "Steven matos""

---
+++ project/project_model.js
@@ -0,0 +1,36 @@
+const db = require("../data/db-config");
+
+module.exports = {
+  addResource,
+  getResources,
+  addProject,
+  getProjects,
+  addTask,
+  getTasks
+};
+
+function addResource(resource) {
+  return db("resources").insert(resource, "id");
+}
+
+function getResources() {
+  return db("resources");
+}
+
+function addProject(project) {
+  return db("projects").insert(project, "id");
+}
+
+function getProjects() {
+  return db("projects");
+}
+
+function addTask(task) {
+  return db("task").insert(task, "id");
+}
+
+function getTasks() {
+  return db("task as t")
+    .join("projects as p", "t.project_id", "p.id")
+    .select("p.name", "p.desc", "t.id", "t.desc", "t.notes", "t.task_complete");
+}
